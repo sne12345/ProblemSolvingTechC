5f1aea6e7bfaf2940a317fb5a17df447a27ae046
sne12345 <sne12345@naver.com>
2022-03-18 14:21:56 +0900

    LAB_comb_count_recur code & result

---
+++ Week2/LAB_comb_count_recur/main.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int comb(int n, int r){
+    if(r == 0 || r == n){
+        return 1;
+    } else {
+        return comb(n-1, r-1) + comb(n-1, r);
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    int C10_5;
+    C10_5 = comb(10, 5);
+    printf("C10_5 = %d\n", C10_5);
+    
+    return 0;
+}

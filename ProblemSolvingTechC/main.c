61f8f41a7484b8d78ac16d2c165af97f02887ae9
sne12345 <sne12345@naver.com>
2022-03-18 14:11:05 +0900

    Initial Commit

---
+++ ProblemSolvingTechC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ProblemSolvingTechC
+//
+//  Created by LG on 2022/03/18.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

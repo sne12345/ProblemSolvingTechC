b0b8bd3db45a870907a7e945e0f3aa6054542180
sne12345 <sne12345@naver.com>
2022-03-19 13:24:39 +0900

    ADD LAB&HW screen tree code & result

---
+++ Week2/LAB_screen_tree/main.c
@@ -0,0 +1,52 @@
+
+#define MAX_LEVEL 5
+#define MAX_WIDTH 70
+char screen[MAX_LEVEL][MAX_WIDTH];
+
+#include <stdio.h>
+
+void init(char s[][MAX_WIDTH]){
+    int i, j;
+    for(i=0; i<MAX_LEVEL; i++){
+        for(j=0; j<MAX_WIDTH; j++){
+            s[i][j] = '-';
+        }
+    }
+}
+
+void display(char s[][MAX_WIDTH], int width){
+    int i, j;
+    for(i=0; i<MAX_LEVEL; i++){
+        for(j=0; j<width; j++){
+            printf(" %c", s[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void draw_tree(int row, int left, int right){
+    int mid;
+    
+    if(right - left < 2) return;
+    
+    mid = (right + left) / 2;
+    
+    screen[row][mid] = 'X';
+    draw_tree(row+1, left, mid-1);
+    draw_tree(row+1, mid+1, right);
+}
+
+int main(int argc, const char * argv[]) {
+    int width;
+    
+    init(screen);
+    
+    printf("Enter a width(<= 70) of a screen: ");
+    scanf("%d", &width);
+    
+    draw_tree(0, 0, width - 1);
+    display(screen, width);
+
+    
+    return 0;
+}
